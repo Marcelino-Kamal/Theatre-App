9fb10c710da56cb523fd64217af3df8ef4a13cb8
Marcelino <marconaoum@gmail.com>
2025-06-23 00:48:54 +0300

    Services

---
+++ Theatre App/Service/UsersServices/UserService.cs
@@ -0,0 +1,14 @@
+﻿using Theatre_App.Repository.UserRepo;
+
+namespace Theatre_App.Service.UsersServices
+{
+    public class UserService
+    {   
+        private readonly UserRepo _userRepo;
+
+        public UserService(UserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+    }
+}
