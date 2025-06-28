93fbfcf41f81e2222b364eb7d005509d47ad0e1f
Marcelino <marconaoum@gmail.com>
2025-06-28 16:41:06 +0300

    Services

---
+++ Theatre App/Service/AuthServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Theatre_App.DTO;
+
+namespace Theatre_App.Service.AuthServices
+{
+    public interface IAuthService
+    {
+        Task<String> Register(UserSignUpDto userdto);
+        Task<String> Login(UserLoginDto userdto);
+    }
+}
