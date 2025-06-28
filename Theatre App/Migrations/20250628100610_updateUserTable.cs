dd464c598df92f705ab9b7b52bc3de076b6a18b8
Marcelino <marconaoum@gmail.com>
2025-06-28 16:40:07 +0300

    AltType

---
+++ Theatre App/Migrations/20250628100610_updateUserTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Theatre_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("d1c1c31e-1234-4fae-8c1c-abcdef123456"),
+                column: "PhoneNumber",
+                value: "01282771887");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("d1c1c31e-1234-4fae-8c1c-abcdef123456"),
+                column: "PhoneNumber",
+                value: 1282771887);
+        }
+    }
+}
