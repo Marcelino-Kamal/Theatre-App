19b60031ca6f982b4127326ed194e709eb99ff9b
Marcelino <marconaoum@gmail.com>
2025-06-28 16:40:27 +0300

    Helper

---
+++ Theatre App/Helpers/PasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Theatre_App.Helpers
+{
+    public class PasswordHelper
+    {
+
+        private static readonly PasswordHasher<object> _hasher = new();
+
+        
+        public static string HashPassword(string password)
+        {
+            return _hasher.HashPassword(null, password);
+        }
+
+        
+        public static bool VerifyPassword(string hashedPassword, string inputPassword)
+        {
+            var result = _hasher.VerifyHashedPassword(null, hashedPassword, inputPassword);
+            return result == PasswordVerificationResult.Success;
+        }
+    }
+}
